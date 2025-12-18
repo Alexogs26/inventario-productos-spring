ee8211f42c45e0a665f6b76996aac3a339990ace
Alex <oswaldoalexisgomezsanchez@gmail.com>
2025-12-18 12:59:08 -0600

    Add ProductService Interface

---
+++ src/main/java/ag/inventario_spring/service/ProductService.java
@@ -0,0 +1,15 @@
+package ag.inventario_spring.service;
+
+import ag.inventario_spring.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    public List<Product> listProducts();
+
+    public Product getProducBySku(Product product);
+
+    public void saveProduct(Product product);
+
+    public void deleteProduct(Product product);
+}
