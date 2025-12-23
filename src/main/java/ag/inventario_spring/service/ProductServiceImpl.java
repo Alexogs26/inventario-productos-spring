78613446ba28a69d76810f35c7d1bde4db2ddf4f
Alex <oswaldoalexisgomezsanchez@gmail.com>
2025-12-22 21:17:02 -0600

    Refactor ProductServiceImpl

--- src/main/java/ag/inventario_spring/service/ProductServiceImpl.java
+++ src/main/java/ag/inventario_spring/service/ProductServiceImpl.java
@@ -2,34 +2,40 @@ package ag.inventario_spring.service;
 
 import ag.inventario_spring.model.Product;
 import ag.inventario_spring.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService{
-    @Autowired
-    private ProductRepository productRepository;
+
+    private final ProductRepository productRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Product> listProducts() {
         List<Product> products = productRepository.findAll();
         return products;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Product getProducBySku(Integer sku) {
         Product product = productRepository.findById(sku).orElse(null);
         return product;
     }
 
     @Override
+    @Transactional
     public void saveProduct(Product product) {
         productRepository.save(product);
     }
 
     @Override
+    @Transactional
     public void deleteProduct(Product product) {
         productRepository.delete(product);
     }
